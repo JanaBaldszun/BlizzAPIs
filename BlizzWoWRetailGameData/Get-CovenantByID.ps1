bc0fc161ad972066f3d26e93292083a37b06b29a
Jana Baldszun <jbaldszun@gmail.com>
2023-12-25 12:35:52 +0100

    Changed Verb from Request to Get

---
+++ BlizzWoWRetailGameData/Get-CovenantByID.ps1
@@ -0,0 +1,26 @@
+﻿function Get-CovenantByID
+{
+  param(
+    [Parameter(Mandatory, Position=0)][String]
+    $ID
+  )
+
+  $EndpointPath = "data/wow/covenant/$ID"
+  $Namespace = -join('?namespace=static-',$Global:WoWRegion,'&locale=',$Global:WoWLocalization,'&')
+  $URL = -join($Global:WoWBaseURL,$EndpointPath,$Namespace,'access_token=',$Global:WoWAccessToken)    
+  
+  try 
+  {
+    $result = Invoke-RestMethod -Uri $URL -TimeoutSec 5
+    if($result) 
+    {
+      return $result
+    }
+  }
+  catch 
+  {
+    $statusCode = $_.Exception.Response.StatusCode.value__
+    $status = $_.Exception.Response.StatusCode
+    return "Bad status code ($statusCode) $status"
+  }
+}
